fbb5637b498df02e32819f6d67d3c3228ac1ecb0
manish kumar <manishkumarstudent8744@gmail.com>
2026-05-02 15:40:47 +0530

    employe speaks

--- app/employee-speaks/page.jsx
+++ app/employee-speaks/page.jsx
@@ -80,7 +80,7 @@ const page = () => {
   return (
     <div>
       <>
-        <section className="relative sb w-full h-[45vh] sm:h-[45vh] md:h-[60vh] bg-[#1A4D6E] flex items-center overflow-hidden">
+        <section className="relative sb w-full h-[45vh] sm:h-[45vh] md:h-[40vh] lg:h-[70vh] bg-[#1A4D6E] flex items-center overflow-hidden">
           {/* Background Circles */}
           <div className="absolute right-[-100px] top-[-80px] w-[400px] h-[400px] bg-white/5 rounded-full"></div>
           <div className="absolute right-[140px] bottom-[10px] w-[300px] h-[300px] bg-white/5 rounded-full"></div>
